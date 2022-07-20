ac0245ca5a87c795c8ddf6612b29a3ef83022fc8
Gilgamesh <ronansaantos@gmail.com>
2022-07-19 23:08:44 -0300

    criacao data class Banco e pacote de testes

--- 02. Fundamentos POO com Kotlin/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ 02. Fundamentos POO com Kotlin/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,11 +4,14 @@ class Pessoa {
     var nome: String = "Ronan"
     var cpf: String = "123.123.123-11"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val ronan = Pessoa()
 
-    println(ronan.nome)
-    println(ronan.cpf)
+    println(ronan.pessoaInfo())
 }

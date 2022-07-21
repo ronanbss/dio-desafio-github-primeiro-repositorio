07388434ac5e32c277f86e8c07a493f6b869b78a
Gilgamesh <ronansaantos@gmail.com>
2022-07-20 22:22:19 -0300

    atualizacao para classe abstrata Pessoa e Funcionario

--- 02. Fundamentos POO com Kotlin/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ 02. Fundamentos POO com Kotlin/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,17 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Ronan"
-    var cpf: String = "123.123.123-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val ronan = Pessoa()
-
-    println(ronan.pessoaInfo())
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
